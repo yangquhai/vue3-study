81eb76953403465649e41950d967c94548deac79
杨屈海 <310582848@qq.com>
2023-08-18 16:21:01 +0800

    init

--- src/utils/request/index.js
+++ src/utils/request/index.js
@@ -16,9 +16,9 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
     (config) => {
-        document.cookie = 'anywell_ajax_token=20230817190506379569'
-        document.cookie = ".ASPXANONYMOUS=AoomSn5ybu_YqtIKURF8k7NOf-FB9gBx6wftQNmcBcJJZ9x0tjfh0bFzSLzxB56PzZzDYvOrCqTkWSQapvziTJybDpZEurZ5PrqujLhLFp7B9V1MU0CBO3uect-l_N3OkGsladizcEGMprG-yi0tfA2"
-        document.cookie = "ASP.NET_SessionId=toybnhtana0fsftnwqsnpihd"
+        // document.cookie = 'anywell_ajax_token=20230817190506379569'
+        // document.cookie = ".ASPXANONYMOUS=AoomSn5ybu_YqtIKURF8k7NOf-FB9gBx6wftQNmcBcJJZ9x0tjfh0bFzSLzxB56PzZzDYvOrCqTkWSQapvziTJybDpZEurZ5PrqujLhLFp7B9V1MU0CBO3uect-l_N3OkGsladizcEGMprG-yi0tfA2"
+        // document.cookie = "ASP.NET_SessionId=toybnhtana0fsftnwqsnpihd"
         return config
     },
     (error) => {
