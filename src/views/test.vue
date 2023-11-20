845f345d8d67841d76cfe564722153dc8e1fd5f7
‘yangquhai’ <310582848@qq.com>
2023-11-20 16:22:26 +0800

    test

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,12 +1,16 @@
 <script setup>
 import { useRouter } from "vue-router";
+import { ref, reactive, computed, watch, onMounted } from 'vue'
 const router = useRouter()
 const msg  = 'msgs'
 const add = () =>{
     console.log(msg2.value)
-    router.push('/404')
+    msg2.value = '2345'
+    arrey.value.push('4')
+    // router.push('/404')
 }
-const msg2 = ref('')
+const msg2 = ref('123')
+const arrey = ref(['1','2','3'])
 // function test(params) {
 //     this.params = params
 //     // console.log(this.params)
@@ -16,12 +20,13 @@ const msg2 = ref('')
 // }
 // const yuanxing = new test(2223)
 // // yuanxing.params
-// console.log(yuanxing.params)1
+// console.log(yuanxing.params)
 
 </script>
 
 <template>
-    <div @click="add">123123 {{ msg }} </div>
+    <div @click="add">123123 {{ msg }} {{ msg2 }} </div>
+    <div v-for="item in arrey">{{ item }}</div>
     <!-- <div @click="test(222)">123</div> -->
 </template>
 
